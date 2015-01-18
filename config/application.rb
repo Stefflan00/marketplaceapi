4b9e8d7a9e43f71a94cde5ed04386ca91bb68c2f
Stefan Maier <stefan.maier09@gmail.com>
2015-01-18 15:12:10 +0100

    adds devise user model

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,9 @@ Bundler.require(*Rails.groups)
 module MarketPlaceApi
   class Application < Rails::Application
 
+    # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      g.test_framework :rspec, :fixture: true
+      g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
       g.view_specs false
       g.helper_specs false
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-require 'api_contraints'
+require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  devise_for :users
 # => Api definition
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
     scope module: :v1 do
