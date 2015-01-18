1c33e7fb00937e9bb08ae8922f2d552ea19df27b
Stefan Maier <stefan.maier09@gmail.com>
2015-01-18 17:30:25 +0100

    adds all actions to users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :get_user, only: [:show, :update, :destroy]
   respond_to :json
 
   def show
-    respond_with User.find(params[:id])
+    respond_with @user
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       render json: user, status: 201, location: [:api, user]
     else
@@ -14,9 +15,26 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+
+    if @user.update(user_params)
+      render json: @user, status: 200, location: [:api, @user]
+    else
+      render json: {errors: @user.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head 204
+  end
+
   private
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 
+    def get_user
+     @user = User.find(params[:id])
     end
 end
