9180474f9c33434642ffaa392051bc0f565b5e7c
Stefan Maier <stefan.maier09@gmail.com>
2015-01-18 17:59:57 +0100

    chapter 3 finished

--- lib/spec/api_constraints_spec.rb
+++ lib/spec/api_constraints_spec.rb
@@ -7,13 +7,13 @@ describe ApiConstraints do
   describe "matches?" do
 
     it "returns true when the version matches the 'Accept' header" do
-      request = double(host: 'api.market_place_api.dev',
+      request = double(host: 'api.marketplaceapi.dev',
                         headers: {"Accept" => "application/vnd.marketplace.v1"})
       api_constraints_v1.matches?(request).should be_true
     end
 
     it "returns the default version when 'default' option is specified" do
-      request = double(host: 'api.market_place_api.dev')
+      request = double(host: 'api.marketplaceapi.dev')
       api_constraints_v2.machtes(request).should be_true
     end
 
