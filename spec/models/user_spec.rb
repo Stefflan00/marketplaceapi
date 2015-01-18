813b1ac2fc1ce5b44f57a79f4c586d1d407851e1
Stefan Maier <stefan.maier09@gmail.com>
2015-01-18 15:26:00 +0100

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,8 @@ require 'rails_helper'
 describe User do
   before {@user = FactoryGirl.build(:user)}
   subject {@user}
-
-  it {should respond_to(:email)}
-  it {should respond_to(:password)}
-  it {should respond_to(:password_confirmation)}
-
-  it {should be_valid}
-
+  it {should validate_presence_of(:email)}
+  it {should validate_uniqeness_of(:email)}
+  it {should validate_confirmation_of(:password)}
+  it {should allow_value('ecample@domain.com').for(:email)}
 end
