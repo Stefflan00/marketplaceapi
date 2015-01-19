d4777bdda0ab02ea325046dee2eb79a1b6841524
Stefan Maier <stefan.maier09@gmail.com>
2015-01-19 12:28:32 +0100

    rspec issue

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module MarketPlaceApi
   class Application < Rails::Application
 
+    config.autoload_paths += %W(#{config.root}/lib)
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
       g.test_framework :rspec, fixture: true
@@ -27,7 +28,7 @@ module MarketPlaceApi
       g.helper = false
     end
 
-    config.autoload_paths += %W(\#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
